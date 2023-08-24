9ae9cdc66c15f76b7d7f67307830e97239b9687d
Dratatin <matchirouze@orange.fr>
2023-08-24 22:00:58 +0200

    about page responsive

--- src/components/avatarBanner/AvatarBanner.jsx
+++ src/components/avatarBanner/AvatarBanner.jsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import {ReactComponent as Star} from '../../assets/star.svg'
 import "./_avatarBanner.scss"
 
@@ -7,19 +8,19 @@ const AvatarBanner = () => {
     return(
         <div className="avatar-banner">
             <div className="avatar-banner__content">
-                {banner.map(item => (
-                    <>
+                {banner.map((item, index) => (
+                    <Fragment key={index}>
                         <span className="avatar-banner__content__label">{item}</span>
                         <Star className="avatar-banner__content__icon" width={22} height={22}/>
-                    </>
+                    </Fragment>
                 ))}
             </div>
             <div className="avatar-banner__content">
-                {banner.map(item => (
-                    <>
+                {banner.map((item, index) => (
+                    <Fragment key={index}>
                         <span className="avatar-banner__content__label">{item}</span>
                         <Star className="avatar-banner__content__icon" width={22} height={22}/>
-                    </>
+                    </Fragment>
                 ))}
             </div>
         </div>
